c61d8dbd0eb0ea00607ebe93443faaf7ffd193df
Haris <au755228@uni.au.dk>
2026-09-19 13:18:32 +0200

    setup the spring boot server using spring inizializr. Added the postgresdb connection and tested the connection by creating a simple User entity.

---
+++ analyzer-auth-server/src/main/java/com/haris/analyzer_auth_server/AnalyzerAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.haris.analyzer_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzerAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzerAuthServerApplication.class, args);
+	}
+
+}
---
+++ analyzer-auth-server/src/main/java/com/haris/analyzer_auth_server/user/User.java
@@ -0,0 +1,28 @@
+package com.haris.analyzer_auth_server.user;
+
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String passwordHash; 
+
+    private List<String> analysisSummaries;
+    
+    // getters/setters or use Lombok @Data to cut boilerplate 
+}
